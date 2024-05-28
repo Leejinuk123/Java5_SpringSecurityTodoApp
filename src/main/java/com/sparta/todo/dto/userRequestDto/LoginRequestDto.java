7027c2e6956d3becef17deabf19df63e7a7a734f
leejinuk <leejinouk123@kakao.com>
2024-05-28 18:50:10 +0900

    refactor: 로그인Dto Valid 검사 패턴 적용

--- src/main/java/com/sparta/todo/dto/userRequestDto/LoginRequestDto.java
+++ src/main/java/com/sparta/todo/dto/userRequestDto/LoginRequestDto.java
@@ -1,9 +1,12 @@
 package com.sparta.todo.dto.userRequestDto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
+    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "사용자명은 소문자와 숫자로만 구성된 4자 이상 10자 이하이어야 합니다.")
     private String username;
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "비밀번호는 대소문자와 숫자로만 구성된 8자 이상 15자 이하이어야 합니다.")
     private String password;
 }
