bfcfcc734b5b0ff5db6ee16c18b7c88a7047654f
leejinuk <leejinouk123@kakao.com>
2024-05-30 13:24:10 +0900

    delete: TodoDeleteRequestDto.java 삭제

--- src/main/java/com/sparta/todo/dto/todoRequest/TodoDeleteRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.todo.dto.todoRequest;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-@Getter
-public class TodoDeleteRequestDto {
-    @NotNull
-    private String password;
-}
